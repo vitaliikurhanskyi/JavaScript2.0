3c270816967ca7bfe7dbea5d573f5b9392c08a51
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-06-08 13:17:26 +0300

    08.06.2022

--- js/script.js
+++ js/script.js
@@ -348,6 +348,244 @@ createNewElement.onclick = function () {
 // Lesson 10
 // Массивы
 
+let testMassive = ['Vasy', 21, 'SEO'];
+document.querySelector('.massive-out').innerHTML = testMassive;
+
+let createNewMassive = document.createElement('div');
+createNewMassive.classList.add('new-massive');
+
+for (let i = 1; i < testMassive.length; i++) {
+	createNewMassive.innerHTML += `<span>${testMassive[0]}</span><br>`;
+	createNewMassive.innerHTML += testMassive[i] + '<br>';
+}
+
+document.querySelector('.massive-out').appendChild(createNewMassive);
+
+//console.log(createNewMassive);
+
+//Методы pop, push,splice
+
+const m1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const m2 = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+//console.log(m1.length);
+//console.log(m1.push(10));
+
+// const test = ['Vasy'];
+
+// for (let i = 0; i < m2.length; i++) {
+// 	test.push(m2[i]);
+// }
+// console.log(test);
+
+//console.log(m2.pop());
+
+//delete m2[3];
+
+// console.log(m2);
+// console.log(m2[3]);
+
+// if (!m2[3]) {
+// 	console.log('if');
+// } else {
+// 	console.log('else');
+// }
+
+//console.log(m2.splice(2, 3));
+//console.log(m2);
+
+// console.log(m2.splice(2, 3, 'add', 'add2'));
+// console.log(m2);
+
+//shift
+//unshift
+
+let d1 = ['test'];
+
+function f1() {
+	if (document.querySelector("#i-1").value === '') {
+		d1.push('empty');
+		document.querySelector("#m1-out").innerHTML = d1;
+		return;
+	}
+	d1.push(document.querySelector("#i-1").value);
+	document.querySelector("#m1-out").innerHTML = d1;
+
+}
+
+
+document.querySelector("#b-1").onclick = f1;
+
+// Двумерные массивы
+
+let massive2d = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+];
+let massiveOut = document.querySelector('.massive2d-out');
+for (let i = 0; i < massive2d.length; i++) {
+	for (let k = massive2d.length - 1; k >= 0; k--) {
+		if (massive2d[i][k] > 4) {
+			massiveOut.innerHTML += massive2d[i][k] + ' ';
+		}
+	}
+	massiveOut.innerHTML += "<br>";
+}
+
+let massive2 = [1, 0, 0, 0, 0];
+let massive2k = 0;
+let massive2Out = document.querySelector('.massive2d-out-2');
+
+let moveBtn = document.querySelector("#moveOne");
+massive2Out.innerHTML = massive2;
+
+//console.log(massive2.length);
+
+
+moveBtn.onclick = function () {
+	//console.log(massive2k);
+	//console.log(massive2.length);
+	if (massive2k <= massive2.length - 2) {
+		massive2[massive2k] = 0;
+		massive2[massive2k + 1] = 1;
+		massive2k++;
+		massive2Out.innerHTML = massive2;
+	} else {
+		massive2[massive2k] = 0;
+		massive2[massive2k - 1] = 1;
+		massive2k--;
+		massive2Out.innerHTML = massive2;
+	}
+}
+
+//Массивы (Обьект)
+
+const obj = {
+	"a": 5,
+	"b": 'Hello',
+	"r": 'Hi'
+}
+
+// console.log(obj);
+// console.log(obj.a);
+// let objkey = 'r';
+// console.log(obj[objkey]);
+
+let outObj = document.querySelector(".out-object");
+
+let outIn = '';
+
+for (let key in obj) {
+	outIn += obj[key];
+}
+
+outObj.innerHTML = outIn;
+
+//Получаем прогноз погоды по API
+//https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a868550f20df98f37e4926673203701d
+
+//https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=a868550f20df98f37e4926673203701d
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=kaniv&appid=a868550f20df98f37e4926673203701d')
+// 	.then(function (resp) { return resp.json() }) // Convert data to json
+// 	.then(function (data) {
+// 		let weatherData = data;
+// 		//console.log(weatherData);
+// 		document.querySelector('.city').textContent = data.name;
+// 		document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273) + "&deg;";
+// 		document.querySelector('.disclaimer').textContent = data.weather[0].description;
+// 		document.querySelector('.disclaimer').style.color = 'aqua';
+// 		document.querySelector('.features').innerHTML = '<li><img src="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png"></li>';
+// 		//console.log(data.weather[0]['icon']);
+// 		//https://openweathermap.org/img/wn/04d@2x.png
+// 	})
+// 	.catch(function () {
+// 		// catch eny errors
+// 	});
+
+
+
+//	Set в JavaScript
+
+let mySet = new Set();
+
+mySet.add('Hello Word!');
+
+mySet.add(1001);
+
+mySet.add('test');
+
+//mySet.clear();
+
+mySet.delete('test');
+
+//console.log(mySet);
+
+//console.log(mySet.size); // количество елементов
+
+//console.log(mySet.has(1001)); // проверка на наличие елемента
+
+for (let item of mySet) {
+	//console.log(item);
+}
+
+let arr = [1, 2, 1, 4, 5, 6, 2, 'hello'];
+
+let newSet = new Set(arr);
+
+//console.log(newSet);
+
+let setArray = Array.from(newSet);
+
+//console.log(setArray);
+
+//console.log(setArray[1]);
+
+//task
+
+// let addset = new Set();
+
+// document.querySelector('#addtoset').onclick = function () {
+// 	let inputValue = document.querySelector('#inputtonewset').value;
+// 	addset.add(inputValue);
+// 	console.log(addset);
+// }
+
+let addset = new Set();
+
+addset.add(1);
+addset.add(2);
+addset.add(3);
+addset.add("World");
+addset.add('Hello');
+
+console.log(addset);
+
+let validInp = '[0-9]';
+
+// document.querySelector('#addtoset').onclick = function () {
+// 	let inputValue = document.querySelector('#inputtonewset').value;
+// 	let reqExp = new RegExp(validInp);
+// 	if (reqExp.test(inputValue)) {
+// 		addset.delete(+inputValue);
+// 	} else {
+// 		addset.delete(inputValue);
+// 	}
+// 	//(reqExp.test(inputValue)) ? addset.delete(+inputValue) : addset.delete(inputValue);
+// 	console.log(addset);
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
